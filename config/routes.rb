4eca242cb2cf0d17a0e0d32f68893de226fb6efa
Dagogo Tolofari <tolofaridagogo@gmail.com>
2022-06-04 17:45:48 +0000

    Added about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
     root 'articles#index'
-    #get '/articles", to: "articles#index'
+    
+    get 'about', to: 'articles#about'
+    #get '/articles', to: 'articles#index'
 end
